57cf01d77912a661a113cc7560bf0a746868518a
jianbiao.zhou <jianbiao.zhou@mxplayer.in>
2023-09-07 11:48:26 +0800

    add define const

---
+++ src/06_const.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    const int WIDTH  = 3;
+    const int LENGTH = 5;
+    int area;
+    area = WIDTH * LENGTH;
+
+    printf("area is %d", area);
+    return 0;
+}
